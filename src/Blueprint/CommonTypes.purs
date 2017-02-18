6642c0ee0aa997107e218e3c479c2a63b2a6861a
Sudhir Kumar <s@atomicits.com>
2017-02-19 02:29:52 +0530

    commontypes formatted and fieldnames changed

--- src/Blueprint/CommonTypes.purs
+++ src/Blueprint/CommonTypes.purs
@@ -2,30 +2,31 @@ module Bluprint.CommonTypes where
 
 newtype Position = Position Int
 
-position :: { top_left :: Position
-            , top :: Position
-            , top_right :: Position
-            , right_top :: Position
-            , right :: Position
-            , right_bottom :: Position
-            , bottom_right :: Position
-            , bottom :: Position
-            , bottom_left :: Position
-            , left_bottom :: Position
-            , left :: Position
-            , left_top :: Position
-            }
-
-position = { top_left :  Position 0
-            , top :  Position 1
-            , top_right :  Position 2
-            , right_top :  Position 3
-            , right :  Position 4
-            , right_bottom :  Position 5
-            , bottom_right :  Position 6
-            , bottom :  Position 7
-            , bottom_left :  Position 8
-            , left_bottom :  Position 9
-            , left :  Position 10
-            , left_top :  Position 11
-            }
+position ::
+  { topLeft :: Position
+  , top :: Position
+  , topRight :: Position
+  , rightTop :: Position
+  , right :: Position
+  , rightBottom :: Position
+  , bottomRight :: Position
+  , bottom :: Position
+  , bottomLeft :: Position
+  , leftBottom :: Position
+  , left :: Position
+  , leftTop :: Position
+  }
+position =
+  { topLeft :  Position 0
+  , top :  Position 1
+  , topRight :  Position 2
+  , rightTop :  Position 3
+  , right :  Position 4
+  , rightBottom :  Position 5
+  , bottomRight :  Position 6
+  , bottom :  Position 7
+  , bottomLeft :  Position 8
+  , leftBottom :  Position 9
+  , left :  Position 10
+  , leftTop :  Position 11
+  }
