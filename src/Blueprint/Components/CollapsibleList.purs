31400c25fe4d579a6d819669aa459bfe2c74344a
Sudhir Kumar <s@atomicits.com>
2017-02-19 02:49:39 +0530

    formatted

--- src/Blueprint/Components/CollapsibleList.purs
+++ src/Blueprint/Components/CollapsibleList.purs
@@ -1,26 +1,33 @@
 module Blueprint.Components.CollapsibleList where
 
-import React
+import Blueprint.Components.Menu (MenuItemProps)
+import Blueprint.Components.Popover (PopoverProps)
 import Blueprint.ComponentsClasses (collapsibleListClass)
-import Blueprint.Components.Menu (IMenuItemProps)
-import Blueprint.PropTypes (IPropsEx, Prop, UnknownJSXElementType)
-import Blueprint.Components.Popover (IPopoverProps)
+import Blueprint.PropTypes (PropsEx, Prop, UnknownJSXElementType)
+import React
 
 newtype CollapseForm = CollapseForm Int
 
-collapseForm  :: { start :: CollapseForm, end :: CollapseForm }
-collapseForm = { start : CollapseForm 0, end : CollapseForm 1}
+collapseForm ::
+  { start :: CollapseForm
+  , end :: CollapseForm
+  }
+collapseForm =
+  { start : CollapseForm 0
+  , end : CollapseForm 1
+  }
 
 type CollapsibleListProps = CollapsibleListPropsEx ()
 
-type CollapsibleListPropsEx r = IPropsEx ( dropdownTarget :: UnknownJSXElementType
-                                         , dropdownProps :: IPopoverProps
-                                         , renderVisibleItem :: IMenuItemProps -> Int -> UnknownJSXElementType
-                                         , collapseFrom :: CollapseForm
-                                         , visibleItemClassName :: String
-                                         , visibleItemCount :: Int
-                                         | r)
-
+type CollapsibleListPropsEx r = PropsEx
+  ( dropdownTarget :: UnknownJSXElementType
+  , dropdownProps :: PopoverProps
+  , renderVisibleItem :: MenuItemProps -> Int -> UnknownJSXElementType
+  , collapseFrom :: CollapseForm
+  , visibleItemClassName :: String
+  , visibleItemCount :: Int
+  | r
+  )
 
 collapsibleList :: Prop CollapsibleListProps -> Array ReactElement -> ReactElement
 collapsibleList = createElement collapsibleListClass
