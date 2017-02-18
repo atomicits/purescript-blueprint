baabdc781392e9df11d3178962a7fa9a80ed3b89
Sudhir Kumar <s@atomicits.com>
2017-02-19 02:33:49 +0530

    formatted

--- src/Blueprint/Components/Button.purs
+++ src/Blueprint/Components/Button.purs
@@ -1,21 +1,22 @@
 module Blueprint.Components.Button where
 
-import React
-import Blueprint.PropTypes (IActionPropsEx, Prop, UnknownRefType)
 import Blueprint.ComponentsClasses (anchorButtonClass, buttonClass)
+import Blueprint.PropTypes (ActionPropsEx, Prop, UnknownRefType)
+import React
 
 type ButtonProps =  ButtonPropsEx ()
-type ButtonPropsEx r =  IActionPropsEx ( elementRef :: UnknownRefType
-                                       , rightIconName :: String
-                                       , loading :: Boolean
-                                       , type :: String
-                                       | r
-                                       )
 
-
-button :: Prop ButtonProps -> Array ReactElement -> ReactElement
-button = createElement buttonClass
-
+-- DISCUSS: Do we need this to be extensible?
+type ButtonPropsEx r = ActionPropsEx
+  ( elementRef :: UnknownRefType
+  , rightIconName :: String
+  , loading :: Boolean
+  , type :: String -- TODO: lets avoid using keywords in field names.
+  | r
+  )
 
 anchorButton :: Prop ButtonProps -> Array ReactElement -> ReactElement
 anchorButton = createElement anchorButtonClass
+
+button :: Prop ButtonProps -> Array ReactElement -> ReactElement
+button = createElement buttonClass
